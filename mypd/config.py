af28f9d8fd177a17f8e4a66516715751229f9036
Paul Miller <paul@jettero.pl>
2022-12-05 17:21:29 -0500

    basic config thingy

--- mypd/config.py
+++ mypd/config.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import os
+from collections import namedtuple
 import yaml
 
+def get_config():
+    with open( os.path.expanduser('~/.mypd.yaml') ) as fh:
+        _in = yaml.safe_load(fh)['mypd']
+        cls = namedtuple('PDC', tuple(sorted(_in)))
+        return cls( **_in )
+
+PDC = get_config()
+del os, namedtuple, yaml
