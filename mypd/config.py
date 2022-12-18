2f85804d183a53125078fff00d15455a3a2cc6f2
Paul Miller <paul@jettero.pl>
2022-12-18 11:28:15 -0500

    make config slightly more flexible

--- mypd/config.py
+++ mypd/config.py
@@ -5,11 +5,31 @@ import os
 from collections import namedtuple
 import yaml
 
-def get_config():
-    with open( os.path.expanduser('~/.mypd.yaml') ) as fh:
+DEFAULT_CONFIG_LOCATION = [
+    '~/.config/mypd/config.yaml',
+    '~/.mypd.yaml',
+]
+
+def get_config(loc=None):
+    if loc is None:
+        e = list()
+        for thing in DEFAULT_CONFIG_LOCATION:
+            try:
+                return get_config(thing)
+            except FileNotFoundError as _e:
+                e.append(_e)
+        raise FileNotFoundError(f"mypd config not found in any of the default locations: {', '.join(DEFAULT_CONFIG_LOCATION)}")
+
+    with open( os.path.expanduser(loc) ) as fh:
         _in = yaml.safe_load(fh)['mypd']
         cls = namedtuple('PDC', tuple(sorted(_in)))
         return cls( **_in )
 
-PDC = get_config()
-del os, namedtuple, yaml
+def reload_config(loc=None):
+    global PDC
+    try:
+        PDC = get_config(loc=loc)
+    except:
+        PDC = None
+
+reload_config()
