9b2980fc7a39e7b084ac3268c29631743323708b
Paul Miller <paul@jettero.pl>
2023-02-21 22:41:54 -0500

    "improvements"

--- mypd/misc.py
+++ mypd/misc.py
@@ -4,20 +4,51 @@
 import datetime
 import dateparser
 
+from .config import PDC
+
 # import datetime
 # def host_timezone():
 #     return datetime.datetime.now().astimezone().tzinfo
 
-def parse_date(x, in_utc=True, fmt='%Y-%m-%dT%H:%M%Z', utc_to_zulu=True):
+
+def parse_date(x, in_utc=True, fmt="%Y-%m-%dT%H:%M%Z", utc_to_zulu=True):
     """
     by default we take strings like "now" and "-2d" and translate to the stupid ISO 8601 format
     """
 
-    pd = dateparser.parse(x, settings={'RETURN_AS_TIMEZONE_AWARE': True})
+    pd = dateparser.parse(x, settings={"RETURN_AS_TIMEZONE_AWARE": True})
     if in_utc:
         pd = pd.astimezone(datetime.timezone.utc)
     if not fmt:
         return pd
     pd = pd.strftime(fmt)
-    return pd[:-3] + 'Z' if utc_to_zulu and pd.endswith('UTC') else pd
+    return pd[:-3] + "Z" if utc_to_zulu and pd.endswith("UTC") else pd
 
+
+def us_and_them(*items, context="user"):
+    for item in items:
+        if isinstance(item, (list, tuple)):
+            yield from _us_and_them(*item)
+        elif isinstance(item, str):
+            if item in ("all", "any"):
+                if context == "status":
+                    yield from ("triggered", "acknowledged", "resolved")
+            if item in ("me", "mine"):
+                yield PDC.user_id
+            elif item == "them":
+                yield from PDC.team_ids
+            elif item == "us":
+                if context == "user":
+                    yield PDC.user_id
+                elif context == "teams":
+                    yield PDC.team_ids
+
+
+def split_strings_maybe(*items):
+    ret = set()
+    for item in items:
+        if isinstance(item, (list, tuple)):
+            ret.update(_split_strings_maybe(*item))
+        elif isinstance(item, str):
+            ret.add(_us_and_them(*shlex.split(item)))
+    return list(sorted(ret))
--- mypd/raw.py
+++ mypd/raw.py
@@ -2,9 +2,12 @@
 # coding: utf-8
 
 import logging
-from .config import PDC
-from .misc import parse_date
+import shlex
+
 from pdpyras import APISession as PDSession
+
+from .config import PDC
+from .misc import parse_date, us_and_them, split_strings_maybe
 
 SESSION = None
 
@@ -19,29 +22,30 @@ def get_session():
 
     return SESSION
 
-def list_incidents(user_ids=None, team_ids=None, statuses=None, sess=get_session(), since=None, until=None, date_range=None, **params):
-    if user_ids is not None:
-        params["user_ids[]"] = [PDC.user_id if x in ("me", "us", "mine") else x for x in user_ids]
+
+def list_incidents(
+    user_ids=None, team_ids=None, statuses=None, sess=get_session(), since=None, until=None, date_range=None, **params
+):
+    if user_ids is None:
+        params["user_ids[]"] = [PDC.user_id]
+    elif user_ids := split_strings_maybe(user_ids, context="user"):
+        params["user_ids[]"] = user_ids
 
     if since is not None:
-        params['since'] = parse_date(since).strftime('%Y-%m-%dT%H:%M%Z')
+        params["since"] = parse_date(since).strftime("%Y-%m-%dT%H:%M%Z")
 
     if until is not None:
-        params['until'] = parse_date(until).strftime('%Y-%m-%dT%H:%M%Z')
+        params["until"] = parse_date(until).strftime("%Y-%m-%dT%H:%M%Z")
 
-    if team_ids is not None:
-        teams = list()
-        for item in team_ids:
-            if item in ("me", "us", "mine"):
-                teams.extend(PDC.team_ids)
-            else:
-                teams.append(item)
-        params["team_ids[]"] = teams
+    if team_ids is None:
+        params["team_ids[]"] = PDC.team_ids
+    elif team_ids := split_strings_maybe(team_ids):
+        params["team_ids[]"] = team_ids
 
-    if statuses is not None:
-        params["statuses[]"] = list(statuses)
-    else:
+    if statuses is None:
         params["statuses[]"] = ["triggered"]
+    elif statuses := split_strings_maybe(statuses, context="status"):
+        params["statuses[]"] = statuses
 
     log.debug("list_all(params=%s)", params)
 
